18c4b6c0c8ad3f22a3fae73bbbec9ae53c4c63eb
Noam Kraus-Feder <noam.krausfeder@post.runi.ac.il>
2024-11-28 11:38:54 +0200

    finished primes

--- Primes.java
+++ Primes.java
@@ -1,5 +1,21 @@
 public class Primes {
     public static void main(String[] args) {
-        // Replace this statement with your code
+        int n = Integer.parseInt(args[0]);
+        int[] arrnums = new int[n + 1];
+        for (int i = 0; i < arrnums.length; i++) {
+            arrnums[i] = i;
+        }
+        boolean[] arr = new boolean[n + 1];
+        for (int i = 2; i < arr.length; i ++) {
+            arr[i] = true;
+        }
+        for (int i = 2; i < arr.length; i ++) {
+            for (int j = i + 1; j < arr.length; j++) {
+                if (j % i == 0)
+                {
+                    arr[j] = false;
+                }
+            }   
+        }
     }
 }
