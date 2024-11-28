75d85a5d16c9fe60bcada4a8c35c5bdbe153aeb7
Noam Kraus-Feder <noam.krausfeder@post.runi.ac.il>
2024-11-28 12:50:15 +0200

    finished keywordsdetector

--- KeywordsDetector.java
+++ KeywordsDetector.java
@@ -21,6 +21,57 @@ public class KeywordsDetector {
     // Iterates through all the sentences.
     // If a sentence contains one or more of the kewords, prints it.
     public static void detectAndPrint(String[] sentences, String[] keywords) {
-        // Replace this comment with your code
+        for (int i = 0; i < sentences.length; i++) {
+            for (int j = 0; j < keywords.length; j++) {
+                if (contains(sentences[i], keywords[j]) == true) {
+                    System.out.println(sentences[i]);
+                    break;
+                }
+            }
+        }
+    }
+
+    /** Returns the lowercase version of the given string. */
+    public static String lowerCase(String str) {
+        String newstr = "";
+		for (int i = 0; i <= str.length()-1; i++) {
+			char c = str.charAt(i);
+			if ((c >= 65) && (c <= 90)) {
+				c += 32;
+			} 
+			newstr += c;
+		}
+		return newstr;
+    }
+
+    /** If str1 contains str2, returns true; otherwise returns false. */
+    public static boolean contains(String str1, String str2) {
+        String str3 = lowerCase(str1);
+        String str4 = lowerCase(str2);
+		boolean con = false;
+        if (str3.length() < str4.length()) {
+            return false;
+        }
+        // I created a substring of str1, that is the length of str2, and checked each time if the substring
+        // equals str2 using a function strequals that i created to check if the 2 strings are equal
+		for (int i = 0; i <= str3.length()-str4.length(); i++) {
+			if (strEquals(str3.substring(i, (i + (str4.length()))), str4) == true) {
+				con = true;
+			}
+		}
+		return con;
+	}
+
+    // This function checks if 2 strings are equal by comparing the values in their indexes
+    public static boolean strEquals(String str1, String str2) {
+		if (str1.length() != str2.length()) {
+			return false;
+		}
+		for (int i = 0; i < str1.length(); i++) {
+			if (str1.charAt(i) != str2.charAt(i)) {
+				return false;
+			}
+		}
+		return true;
     }
 }
